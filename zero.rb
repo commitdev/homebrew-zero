0490e52acf21b9c742003312ca850ed46c72b203
Commit.dev <contact@commit.dev>
2021-04-08 12:42:19 -0400

    Brew formula update for zero version v0.1.1

--- zero.rb
+++ zero.rb
@@ -2,16 +2,16 @@
 class Zero < Formula
   desc "Allow startup developers to ship to production on day 1."
   homepage "https://github.com/commitdev/zero"
-  version "0.1.0"
+  version "0.1.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/commitdev/zero/releases/download/v0.1.0/zero_0.1.0_Darwin_x86_64.tar.gz"
-    sha256 "a6c3eac25ffb3dfd53f0435eed037b0335c603d08deaac525f086b4df83d3bfb"
+    url "https://github.com/commitdev/zero/releases/download/v0.1.1/zero_0.1.1_Darwin_x86_64.tar.gz"
+    sha256 "89fbc41d73f2c6a7ef30cefeab28038db0cce6c9882bc2b93279f894da41c0d3"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/commitdev/zero/releases/download/v0.1.0/zero_0.1.0_Linux_x86_64.tar.gz"
-    sha256 "6b2e183f33b3bc743e6fa714fc794a31817b8e468c139e57ea8ca2d1e4e3d71c"
+    url "https://github.com/commitdev/zero/releases/download/v0.1.1/zero_0.1.1_Linux_x86_64.tar.gz"
+    sha256 "9b811a11308bba15da9663c6ad155096987bd12e3653b1dc432ece91efd39726"
   end
 
   depends_on "git" => :optional
