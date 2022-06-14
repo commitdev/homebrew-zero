ea1f07b1ecf1c6eee006a223e3f96218f5400d10
Commit.dev <contact@commit.dev>
2022-06-14 13:25:12 -0700

    Brew formula update for zero version v0.2.5

--- zero.rb
+++ zero.rb
@@ -5,20 +5,20 @@
 class Zero < Formula
   desc "Allow startup developers to ship to production on day 1."
   homepage "https://github.com/commitdev/zero"
-  version "0.2.4"
+  version "0.2.5"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/commitdev/zero/releases/download/v0.2.4/zero_0.2.4_Darwin_arm64.tar.gz"
-      sha256 "414381d615e9a9624edee388b2a71de731b708d025445bcda7a6be8873aea4c1"
+      url "https://github.com/commitdev/zero/releases/download/v0.2.5/zero_0.2.5_Darwin_arm64.tar.gz"
+      sha256 "adec84d3a07a58fec1da962d17506faf9d79cf6818ee606ecec2e0fa7da70bcd"
 
       def install
         bin.install "zero"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/commitdev/zero/releases/download/v0.2.4/zero_0.2.4_Darwin_x86_64.tar.gz"
-      sha256 "1440187d7d4c361d85def6ae1bc844a036ecd0e8f1f965ac49ddcfac63170cab"
+      url "https://github.com/commitdev/zero/releases/download/v0.2.5/zero_0.2.5_Darwin_x86_64.tar.gz"
+      sha256 "b90206810b9970efcfea51acb502eba2d9ec072f0f3ae81c46920a19db3021fe"
 
       def install
         bin.install "zero"
@@ -28,16 +28,16 @@ class Zero < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/commitdev/zero/releases/download/v0.2.4/zero_0.2.4_Linux_arm64.tar.gz"
-      sha256 "7fdd232637c1fc02eb494dfc7edeb045b5bd7682c034a4b255dc4f091f4385bf"
+      url "https://github.com/commitdev/zero/releases/download/v0.2.5/zero_0.2.5_Linux_arm64.tar.gz"
+      sha256 "3a55e585934f7ff91ff1c4b67005342b58b26bff4a7aac51ffd9d98a083bfc90"
 
       def install
         bin.install "zero"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/commitdev/zero/releases/download/v0.2.4/zero_0.2.4_Linux_x86_64.tar.gz"
-      sha256 "d47c74ba7d6f346a30a62d41264d59822052d70172c2757f26739147b0666a47"
+      url "https://github.com/commitdev/zero/releases/download/v0.2.5/zero_0.2.5_Linux_x86_64.tar.gz"
+      sha256 "0aa20986fdced20257267e73cbc74a8461e6257681e50e5be5f478c7463e9540"
 
       def install
         bin.install "zero"
